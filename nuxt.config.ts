f6aa556f7cb574ea49da5851f1bcdc018c5d2270
Bartosz 'b4rti' Gorol <bartgorol@t-online.de>
2023-02-02 00:20:41 +0000

    Vuetify3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  app: {
+    baseURL: '/ui',
+  },
+  css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.css' ],
+  build: {
+    transpile: ['vuetify'],
+  },
+  ssr: true,
+  vite: {},
 })
